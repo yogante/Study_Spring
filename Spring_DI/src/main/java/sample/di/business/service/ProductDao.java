e2de143a183972f6ab3981791305b0a6781c7704
yogante <yo_yo_ga@yahoo.co.jp>
2023-11-07 09:42:55 +0900

    no message

--- Spring_DI/src/main/java/sample/di/business/service/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package main.java.sample.di.business.service;
-
-import main.java.sample.di.business.domain.Product;
-
-public interface ProductDao {
-	void addProduct(Product product);
-	Product findByProductName(String name);
-		
-}
--- Spring_DI/src/main/java/sample/di/business/service/ProductService.java
+++
@@ -1,8 +0,0 @@
-package main.java.sample.di.business.service;
-
-import main.java.sample.di.business.domain.Product;
-
-public interface ProductService {
-	void addproduct(Product product);
-	Product findByProductName(String name);
-}
